165cd30a50d7edb2b259499761c33c0262330f19
fkwilczek <fk.wilczek@gmail.com>
2021-05-11 12:55:50 +0200

    init

---
+++ build/CMakeFiles/Release_bullet_hell_project_0.1.0.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/pliki/moje programy 2/c++/bullet_hell_project/src/main.cpp" "F:/pliki/moje programy 2/c++/bullet_hell_project/build/CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/main.cpp.obj"
+  "F:/pliki/moje programy 2/c++/bullet_hell_project/src/pociski/pocisk.cpp" "F:/pliki/moje programy 2/c++/bullet_hell_project/build/CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/pociski/pocisk.cpp.obj"
+  "F:/pliki/moje programy 2/c++/bullet_hell_project/src/pociski/pocisk_prosty.cpp" "F:/pliki/moje programy 2/c++/bullet_hell_project/build/CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/pociski/pocisk_prosty.cpp.obj"
+  "F:/pliki/moje programy 2/c++/bullet_hell_project/src/pociski/tworzenie_pociskow.cpp" "F:/pliki/moje programy 2/c++/bullet_hell_project/build/CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/pociski/tworzenie_pociskow.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Release_bullet_hell_project_0.1.0.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/Release_bullet_hell_project_0.1.0.exe"
+  "../bin/Release_bullet_hell_project_0.1.0.exe.manifest"
+  "../bin/Release_bullet_hell_project_0.1.0.pdb"
+  "CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/main.cpp.obj"
+  "CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/pociski/pocisk.cpp.obj"
+  "CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/pociski/pocisk_prosty.cpp.obj"
+  "CMakeFiles/Release_bullet_hell_project_0.1.0.dir/src/pociski/tworzenie_pociskow.cpp.obj"
+  "libRelease_bullet_hell_project_0.1.0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Release_bullet_hell_project_0.1.0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
